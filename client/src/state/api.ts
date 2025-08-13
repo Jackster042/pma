cf5af21004c347fdc5f9bf43685e4240cba8d891
Nemanja Stojanovic <feemail042@gmail.com>
2025-08-13 02:36:13 +0200

    frontend - api types

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,5 +1,65 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
+export interface User {
+  userId?: number;
+  email: string;
+  username: string;
+  profilePictureUrl?: string;
+  cognitoId?: string;
+  teamId?: number;
+}
+
+export interface Attachment {
+  id: number;
+  fileURL: string;
+  fileName: string;
+  taskId: number;
+  uploadedById: number;
+}
+
+export interface Project {
+  id: number;
+  name: string;
+  description?: string;
+  startDate?: string;
+  endDate?: string;
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  status?: string;
+  priority?: string;
+  tags?: string;
+  startDate?: string;
+  dueDate?: string;
+  points?: number;
+  projectId?: number;
+  authorUserId?: number;
+  assignedUserId?: number;
+
+  author?: User;
+  assignee?: User;
+  comments?: Comment[];
+  attachments?: Attachment[];
+}
+
+export enum Status {
+  ToDo = "To Do",
+  WorkInProgress = "Work In Progress",
+  UnderReview = "Under Review",
+  Completed = "Completed",
+}
+
+export enum Priority {
+  Urgent = "Urgent",
+  High = "High",
+  Medium = "Medium",
+  Low = "Low",
+  Backlog = "Backlog",
+}
+
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL! }),
   reducerPath: "api",
